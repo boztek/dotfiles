712bc2df4d158d3a84d6f254da25f60b11410e90
Boris Gordon <boris@boztek.net>
2018-07-02 17:55:38 +1000

    Install yarn without node dep

--- init/50_node.sh
+++ init/50_node.sh
@@ -4,4 +4,4 @@ source $DOTFILES/source/50_node.sh init
 # Install latest stable Node.js, set as default, install global npm modules.
 nave_install stable
 
-brew install yarn
+brew install yarn --without-node
