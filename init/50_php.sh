7a6f352f77fe2479a0d3fd02e4031216fd31bdbc
Boris Gordon <EMAIL>
2019-02-17 12:39:18 +1100

    installs a bit cleaner

--- init/50_php.sh
+++ init/50_php.sh
@@ -3,6 +3,7 @@ source $DOTFILES/source/50_php.sh init
 
 # Install PHP stack with valet.
 brew install php@7.2
+
 wget https://raw.githubusercontent.com/composer/getcomposer.org/cb19f2aa3aeaa2006c0cd69a7ef011eb31463067/web/installer -O - -q | php -- --quiet
 mv composer.phar /usr/local/bin/composer
 composer global require laravel/valet
--- source/50_php.sh
+++ source/50_php.sh
@@ -2,3 +2,7 @@ export PATH
 
 # Make sure the ~/.composer/vendor/bin directory is in your system's "PATH".
 PATH="$(path_remove $HOME/.composer/vendor/bin):$HOME/.composer/vendor/bin"
+
+# Use PHP 7.2 CLI
+PATH="/usr/local/opt/php@7.2/bin:$(path_remove /usr/local/opt/php@7.2/bin)"
+PATH="/usr/local/opt/php@7.2/sbin:$(path_remove /usr/local/opt/php@7.2/sbin)"
