80f60e2d7a6fad959c3c1311a2c25fdbb834590a
Boris Gordon <boris@boztek.net>
2019-11-11 23:20:30 +1100

    vmware; vagrant

--- init/10_osx.sh
+++ init/10_osx.sh
@@ -17,3 +17,14 @@ brew install tree
 brew install wget
 
 brew cask install visual-studio-code
+
+# vmware fusion and vagrant
+brew cask install vmware-fusion
+brew cask install vagrant
+if VAGRANT_PLUGIN=$(vagrant plugin list |grep vagrant-vmware-desktop); then
+    e_header "$VAGRANT_PLUGIN already installed"
+else
+    brew cask install vagrant-vmware-utility
+    vagrant plugin install vagrant-vmware-desktop
+    vagrant plugin license vagrant-vmware-desktop .dotfiles/configs/vagrant-vmware-desktop.lic
+fi
